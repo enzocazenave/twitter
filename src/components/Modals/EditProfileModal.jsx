5439baad917856d14df0c5abeb55d4a09a92bcf9
Enzo <encaze24@hotmail.com>
2022-10-04 23:40:24 -0300

    Edit Profile Modal change

--- src/components/Modals/EditProfileModal.jsx
+++ src/components/Modals/EditProfileModal.jsx
@@ -14,7 +14,7 @@ Modal.setAppElement('#root');
 export const EditProfileModal = () => {
     return (
         <Modal 
-            isOpen={ true }
+            isOpen={ false }
             style={ modalStyle }
             className="modal"
             overlayClassName="modal-background"
@@ -24,6 +24,21 @@ export const EditProfileModal = () => {
                 <button className="editmodal-nav_close" type="button"><i className="fa fa-close"></i></button>
                 <h4 className="editmodal-nav_title">Edit profile</h4>
                 <button className="editmodal-nav_edit" type="button">Save</button>
+            </div>
+
+            <div className="editmodal-banner">
+                <div className="editmodal-banner_actions">
+                    <button className="editmodal-banner_actions--action"><i className="fa fa-camera"></i></button>
+                    <button className="editmodal-banner_actions--action"><i className="fa fa-close"></i></button>
+                </div>
+
+                
+            </div>
+
+            <div className="editmodal-profile">
+                <div className="editmodal-banner_actions">
+                    <button className="editmodal-banner_actions--action"><i className="fa fa-camera"></i></button>
+                </div>
             </div>
         </Modal>
     )
