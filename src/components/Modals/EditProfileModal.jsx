af63f5ea88214af1b72cc7c600168beca63da54f
Enzo <encaze24@hotmail.com>
2022-10-04 22:01:40 -0300

    Add nav of edit profile modal

--- src/components/Modals/EditProfileModal.jsx
+++ src/components/Modals/EditProfileModal.jsx
@@ -14,14 +14,16 @@ Modal.setAppElement('#root');
 export const EditProfileModal = () => {
     return (
         <Modal 
-            isOpen={ false }
+            isOpen={ true }
             style={ modalStyle }
             className="modal"
             overlayClassName="modal-background"
             closeTimeoutMS={ 200 }
         >
-            <div className="tweetmodal-nav">
-                <button className="tweetmodal-nav_button" type="button"><i className="fa fa-close"></i></button>
+            <div className="editmodal-nav">
+                <button className="editmodal-nav_close" type="button"><i className="fa fa-close"></i></button>
+                <h4 className="editmodal-nav_title">Edit profile</h4>
+                <button className="editmodal-nav_edit" type="button">Save</button>
             </div>
         </Modal>
     )
